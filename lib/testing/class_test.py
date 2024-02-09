ce9aaa81017bfe8095a222a67bdbebd8716a9d86
Benson Mwangi <kimaniben@gmail.com>
2024-02-09 14:06:16 +0000

    Completed assignment

--- lib/testing/class_test.py
+++ lib/testing/class_test.py
@@ -1,14 +1,11 @@
 from lib.config import CURSOR
 from lib.song import Song
-
 class TestClass:
     '''Class Song in song.py'''
-
     def test_creates_songs_table(self):
         '''has classmethod "create_table()" that creates a table "songs" if table does not exist.'''
         Song.create_table()
         assert(CURSOR.execute("SELECT * FROM songs"))
-
     def test_initializes_with_name_and_album(self):
         '''takes a name and album as __init__ arguments and saves them as instance attributes.'''
         song = Song("Hold On", "Born to Sing")
@@ -17,7 +14,7 @@ class TestClass:
     def test_saves_song_to_table(self):
         '''has instancemethod "save()" that saves a song to music.db.'''
 
-        
+
         CURSOR.execute('''DROP TABLE IF EXISTS songs''')
         Song.create_table()
 
@@ -28,13 +25,10 @@ class TestClass:
             ('Hold On', 'Born to Sing')
         ).fetchone()
         assert(db_song[1] == song.name and db_song[2] == song.album)
-
     def test_creates_and_returns_song(self):
         '''has classmethod "create()" that creates a Song instance, saves it, and returns it.'''
-
         CURSOR.execute('''DROP TABLE IF EXISTS songs''')
         Song.create_table()
-
         song = Song.create("Hold On", "Born to Sing")
         db_song = CURSOR.execute(
             'SELECT * FROM songs WHERE name=? AND album=?',
